e8caee3c51d79fae43bbf7ebf93bd3884c382e2b
Charles200000 <charles.farhat@hotmail.fr>
2019-08-03 23:01:11 +0200

    ;) LF shits

---
+++ Installer.sh
@@ -0,0 +1,6 @@
+echo "Please check that you've eddited : ProdServer_PIMCore_db with correct passwords"
+echo "Please check that you've eddited : PIMCore/Files/installer.yalm with correct passwords"
+
+read -p "Press enter to continue"
+
+echo "TO DO : faire le launcher !"
--- PIMCore/Installer.sh
+++
@@ -1,6 +0,0 @@
-echo "Please check that you've eddited : ProdServer_PIMCore_db with correct passwords"
-echo "Please check that you've eddited : PIMCore/Files/installer.yalm with correct passwords"
-
-read -p "Press enter to continue"
-
-echo "TO DO : faire le launcher !"
